466a33871f1cba02432ea0676c899ce6430e79c0
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-29 14:23:54 -0400

    feat: adiciona movie-carousel

---
+++ front-criticas-br/src/app/main-page/movie-carousel/movie-carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-carousel',
+  imports: [],
+  templateUrl: './movie-carousel.component.html',
+  styleUrl: './movie-carousel.component.css'
+})
+export class MovieCarouselComponent {
+
+}
