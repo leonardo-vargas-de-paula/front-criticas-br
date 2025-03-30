8b876f33275bdde876d636aaaa1303fbc0be61b9
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-30 16:21:59 -0400

    feat: adiciona carrossel na main page

--- front-criticas-br/src/app/main-page/main-page/main-page.component.ts
+++ front-criticas-br/src/app/main-page/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../shared/components/card/card.component';
+import { MovieCarouselComponent } from '../movie-carousel/movie-carousel.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [CardComponent],
+  imports: [CardComponent, MovieCarouselComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
