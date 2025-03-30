c9d2da35277401d7397ed37d886764459d728a6f
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-30 14:14:58 -0400

    feat:adiciona card na main page

--- front-criticas-br/src/app/main-page/main-page/main-page.component.ts
+++ front-criticas-br/src/app/main-page/main-page/main-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
